dfc7b1f44e45bccd8f6813e2637c125496cb745e
stephen <stephenmacgregor9@gmail.com>
2019-12-17 17:14:39 +0000

    unsplahed image api added/componets

--- App.js
+++ App.js
@@ -1,13 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator,FlatList, Dimensions, Image} from 'react-native';
+import axios from 'axios'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>This worked</Text>
-    </View>
-  );
-}
+const {height,width} = Dimensions.get('window')
+export default class App extends React.Component {
+
+    constructor() {
+      super()
+      this.state = {
+        isLoading: true,
+        images: []
+      };
+
+      this.loadWallpapers = this.loadWallpapers.bind(this)
+      this.renderItem = this.renderItem.bind(this);
+    }
+
+    loadWallpapers(){
+      axios.get(' https://api.unsplash.com/photos/random?count=15&client_id=6a65cda473e6406acc4007af4aff5347416b7baeb8556e63175d99be64f03ed2').then(function(response){
+        console.log(response.data)
+        this.setState({ images:response.data, isLoading: false })
+
+      }.bind(this)
+      ).catch(function(error){
+        console.log(error)
+
+      }).finally(function(){
+        console.log('request completed')
+      })
+    }
+
+    componentDidMount() {
+      this.loadWallpapers()
+    }
+
+    renderItem(image){
+      return(
+        <View style={{ height,width }}>
+          <Image
+            style={{ flex: 1, height: null, width: null }}
+            source={{ uri: image.urls.regular }}
+            resizeMode="cover"
+          />
+        </View>
+      )
+    }
+
+  render() {
+      return this.state.isLoading? (
+        <View style={{ flex: 1, 
+              backgroundColor: 'black',
+              alignItems: 'center',
+              justifyContent: 'center' }}>
+
+        <ActivityIndicator size="large" color="grey"/>
+
+        </View>
+      ) : (
+        <View style={{ flex: 1, backgroundColor: 'black' }}>
+          <FlatList 
+            horizontal
+            pagingEnabled
+            data={this.state.images}
+            renderItem={(({item})=>this.renderItem(item))}
+            keyExtractor={item => item.id}
+          />
+        </View>
+      );
+    }
+  }
 
 const styles = StyleSheet.create({
   container: {
